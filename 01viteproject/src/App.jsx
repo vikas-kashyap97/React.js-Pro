9174ffb05c70108fb663efd9e0c23b9f95851c5b
Vikas Kashyap <vikaskashyaprock@gmail.com>
2024-07-22 14:47:53 +0530

    Add chai.jsx

--- 01viteproject/src/App.jsx
+++ 01viteproject/src/App.jsx
@@ -1,9 +1,13 @@
+import Chai from "./chai"
+
+
 
 function App() {
   
-
   return (
-   <h1>Hello Vite + React | Vikas Kashyap . I learn about the difference between the basic react project and the Vite + React project.</h1>
+    <>
+   <Chai/>
+   </>
   )
 }
 
