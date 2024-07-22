ff56bc4a003b083800229df7529a009e26b761bd
Vikas Kashyap <vikaskashyaprock@gmail.com>
2024-07-22 17:35:24 +0530

    Custom React.js

---
+++ customReact/customreact.js
@@ -0,0 +1,30 @@
+function customRendor (reactElement, container){
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if ( prop === "children") continue;
+        domElement.setAttribute(prop, reactElement[prop])
+    }
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+ }
+
+ const mainContainer = document.querySelector('#root')
+
+customRendor(reactElement, mainContainer)
