c62a6cc331d722c72eb402d21d6b7212cb22183f
Vikas Kashyap <vikaskashyaprock@gmail.com>
2024-07-22 13:54:50 +0530

    custom react added

---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+   <h1>Hello Vite + React | Vikas Kashyap . I learn about the difference between the basic react project and the Vite + React project.</h1>
+  )
+}
+
+export default App
