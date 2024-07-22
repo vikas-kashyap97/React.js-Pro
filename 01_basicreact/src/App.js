a6cadef20343ddd477a6311a9435c6a13eb35d40
Vikas Kashyap <vikaskashyaprock@gmail.com>
2024-07-22 13:52:09 +0530

    create your first component

---
+++ 01_basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <h1>Hello React | Vikas Kashyap . I learn about the difference between the basic react project and the Vite + React project.</h1>
+    
+  );
+}
+
+export default App;
