4632c939296df059f83a6d50e2928b7c330c57b2
Vikas Kashyap <vikaskashyaprock@gmail.com>
2024-07-23 12:43:55 +0530

    Counter app with useState

---
+++ 02_counter/my-vite-project/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  // let counter = 6
+  const addvalue = () => {
+    // counter = counter + 1
+    
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+  const removevalue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addvalue}>Add value</button>
+      <br />
+      <button onClick={removevalue}>Remove value</button>
+    </>
+  );
+}
+
+export default App;
