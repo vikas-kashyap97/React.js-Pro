f69842d898c05e0f471855b1fb594fcbe7646ff8
Vikas Kashyap <vikaskashyaprock@gmail.com>
2024-07-26 13:53:33 +0530

    Theme Switcher (App.,Card,ThemeBtn.jsx, theme,tailwind.config.js, index,App.css)

---
+++ 09themeswitcher/src/contexts/theme.js
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+
+
+
+export const ThemeContext = createContext({
+    themeMode: "Light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
