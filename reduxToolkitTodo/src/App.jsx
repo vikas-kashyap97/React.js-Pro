d046c5c9c0ebc4c317c09ad031e2f41a951df8ae
Vikas Kashyap <vikaskashyaprock@gmail.com>
2024-07-27 15:27:31 +0530

    reduc toolkit project (Todo)

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './componets/AddTodo'
+import Todos from './componets/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
